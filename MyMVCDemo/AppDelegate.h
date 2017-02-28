14ce9a5ee242658d4868f4a0e914d2180caad5e0
vishal <vishal@syneoteksoft.com>
2017-02-28 18:11:21 +0530

    Initial Commit

---
+++ MyMVCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyMVCDemo
+//
+//  Created by Bhimashankar Vibhute on 28/02/17.
+//  Copyright © 2017 Syneotek Software Solution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyMVCDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyMVCDemo
+//
+//  Created by Bhimashankar Vibhute on 28/02/17.
+//  Copyright © 2017 Syneotek Software Solution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
